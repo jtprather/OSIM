7d4145864b0b61ab35001b29948665727a3b5c58
justin prather <prather.justin@gmail.com>
2014-07-18 00:26:02 -0400

    Passing of the first test.

---
+++ Solution1/OSIM.Core/Persistence/ItemTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NHibernate;
+using OSIM.Core.Entities;
+
+namespace OSIM.Core.Persistence
+{
+    public interface IItemTypeRepository
+    {
+        int Save(ItemType itemType);
+    }
+    public class ItemTypeRepository : IItemTypeRepository
+    {
+        private ISessionFactory _sessionFactory;
+        public ItemTypeRepository(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+        public int Save(ItemType itemType)
+        {
+            int id;
+            using (var session = _sessionFactory.OpenSession())
+            {
+                id = (int)session.Save(itemType);
+                session.Flush();
+            }
+            return id;
+        }
+    }
+}
